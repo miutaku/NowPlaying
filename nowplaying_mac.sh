8c27db48e49aca0eeee3911d6a8837cb525aa493
m1u7aku <mtakumi.0925@gmail.com>
2022-01-13 15:33:07 +0900

    artwork and url

--- nowplaying_mac.sh
+++ nowplaying_mac.sh
@@ -13,7 +13,7 @@ osascript -e 'tell application "Music"
         set i_artist to artist of current track
         set i_album to album of current track
 end tell
-log "Song: " & i_name & "\n" & "Artist: " & i_artist & "\n" & "Album: " & i_album & "\n\n#NowPlaying by miutaku_tmiura"' &> ${nowplaying_info}
+log "Song: " & i_name & "\n" & "Artist: " & i_artist & "\n" & "Album: " & i_album & "\n\n#NowPlaying by <@UserID>"' &> ${nowplaying_info}
 
 # get nowplaying url
 osascript -e 'tell application "Music"
@@ -26,12 +26,14 @@ osascript -e 'tell application "Music"
 name=`sed -n 1P ${info}`
 artist=`sed -n 2P ${info}`
 album=`sed -n 3P ${info}`
+encorded_name=`echo "$name" | /opt/homebrew/bin/nkf -WwMQ | tr = %`
+encorded_album=`echo "$album" | /opt/homebrew/bin/nkf -WwMQ | tr = %`
 string="$name+$artist+$album"
+#string="$name+$artist"
 search=`echo ${string} | sed -e "s/ /+/g"`
-#echo ${search} # debug
+echo ${search} # debug
 
-
-curl "https://itunes.apple.com/search?term=${search}&country=JP&lang=ja_jp&media=music" | jq '.results[]| .trackViewUrl' | sed -e 's/"//g' >> ${nowplaying_info}
+curl "https://itunes.apple.com/search?term=${search}&country=JP&lang=ja_jp&media=music" | /opt/homebrew/bin/jq '.results[]| .trackViewUrl' | head -n 1 | sed -e 's/"//g' | grep "https://music.apple.com/jp/album/${encorded_name}/" >> ${nowplaying_info}
 
 # diff info and post to slack
 diff $nowplaying_info $nowplaying_info_old
@@ -40,6 +42,6 @@ if [ $? == 0 ]; then
  exit
 else
  curl -X POST --data-urlencode \
- "payload={\"text\": \"`cat ${nowplaying_info}`\" }" \
+ "payload={\"text\": \"`cat ${nowplaying_info}`\", \"unfurl_links\": true }" \
  ${webhookurl}
 fi
